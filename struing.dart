ba3bf6da36ddbf83c725193d830deb25b25498dc
MARCELO HENRIQUE PEREIRA PAIVA DE SOUZA <marcelo.1925@aluno.pr.senac.br>
2023-04-10 10:32:09 -0300

    kalipso

---
+++ struing.dart
@@ -0,0 +1,11 @@
+void main() {
+  jedi("Dieimes","Gustavo");
+   jedi("Dieimes","Marcelo");
+    jedi("Dieimes","Ana Maria");
+     jedi("Dieimes","Vitoria");
+} 
+
+void jedi(String professor, aluno) {
+  print("O professor $professor é um Padawan");
+  print("O aluno $aluno é um Mestre Jedi");
+}
